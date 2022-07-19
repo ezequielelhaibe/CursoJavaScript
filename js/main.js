998a78193abead2e0396fe80d006d188fe54b3d3
Ezequiel El Haibe <ezequiel.elhaibe@gmail.com>
2022-07-19 00:31:09 -0300

    segundaentregapfinal

--- js/main.js
+++ js/main.js
@@ -1,84 +1,137 @@
 const tarjetas = document.getElementById("tarjetaProducto");
-productosDisponibles.forEach((productosDisponibles,indice)=>{
+productos.forEach((productosDisponibles,indice)=>{
     let card = document.createElement("div");
     card.classList.add("card", "col-sm-12", "col-lg-4")
-    card.innerHTML=`<img src="${productosDisponibles.imagen}" class="card-img-top" alt="imagenproducto">
+    card.innerHTML=`
+        <img src="${productosDisponibles.imagen}" class="card-img-top" alt="imagenproducto">
         <div class="card-body">
         <h5 class="card-title">${productosDisponibles.nombre}</h5>
         <p class="card-text">${productosDisponibles.descripcion}</p>
         <p class="card-text">$ ${productosDisponibles.precio}</p>
         <a href="#" class="btn btn-primary" onClick="agregarCarrito(${indice})">COMPRAR</a>
-        </div>`;
+        </div>
+        `;
     tarjetas.appendChild(card);
 });
 
-const agregarCarrito=(indice)=>{
-    const indiceEncontradoCarrito = carrito.findIndex((elemento)=>{
-        return elemento.id === productosDisponibles[indice].id
-    })
-    if(indiceEncontradoCarrito === -1){
-        const agregarproducto= productosDisponibles[indice];
-        agregarproducto.cantidad = 1
-        carrito.push(agregarproducto);
-        actualizarlocalstorage(carrito);
-        crearcarrito()
-    }
-    else {
-        carrito[indiceEncontradoCarrito].cantidad += 1
-        actualizarlocalstorage(carrito);
-        crearcarrito();
-    }
-};
-
-const modalCarrito = document.getElementById("cajacarrito");
+let cuadrocarrito = document.getElementById("cart");
 let total = 0;
 const crearcarrito = ()=>{
-    modalCarrito.className = "cajacarrito";
-    modalCarrito.innerHTML = "";
-    if(carrito.length > 0){
-        carrito.forEach((productosDisponibles, indice)=> {
+    cuadrocarrito.className = "cart";
+    cuadrocarrito.innerHTML = "";
+    if(cart.length > 0){
+        cart.forEach((productosDisponibles, indice)=> {
             total = total + productosDisponibles.precio * productosDisponibles.cantidad;
-            const carritoConteiner = document.createElement("div");
-            carritoConteiner.className = "cajacarrito";
-            carritoConteiner.innerHTML = `
-            <img class="car-img" scr="${productosDisponibles.imagen}"/>
+            const cajacarrito = document.createElement("div");
+            cajacarrito.className = "producto-carrito";
+            cajacarrito.innerHTML = `
+            <img class="car-img" src="${productosDisponibles.imagen}"/></div>
+            <div class="product-details">${productosDisponibles.nombre}</div>
             <div class="product-details"> Cantidad: ${productosDisponibles.cantidad}</div>
             <div class="product-details"> Precio: $ ${productosDisponibles.precio}</div>
             <div class="product-details">Total: $ ${productosDisponibles.cantidad * productosDisponibles.precio}</div>
             <button class="btn btn-info" id="remove-product" onClick="removeProduct(${indice})">Eliminar</button>
             `;
-            modalCarrito.appendChild(carritoConteiner);
+            cuadrocarrito.appendChild(cajacarrito);
         });
-        const totalfinal = document.createElement("div");
-            totalfinal.className = "total-carrito";
-            totalfinal.innerHTML = `
-            <div class="totaltotal"> TOTAL: $ ${total}</div>
-            <button class="btn btn-info" id="finalizarcompra" onClick="finalizarcompra()">FINALIZAR COMPRA</button>
+            const totalcuadrocarrito = document.createElement("div");
+            totalcuadrocarrito.className = "total-carrito";
+            totalcuadrocarrito.innerHTML = `
+            <div class="total"> TOTAL: $ ${total}</div>
+            <button class="btn btn-info finalizar" id="finalizar" onClick="finalizarcompra()"> FINALIZAR COMPRA </button>
             `;
-        modalCarrito.appendChild(carritoConteiner)
+            cuadrocarrito.appendChild(totalcuadrocarrito)
         }
         else{
-            modalCarrito.classList.remove("carrito");
+            cuadrocarrito.classList.remove("cart");
         }
 };
 
-const actualizarlocalstorage = (carrito)=>{
-    localStorage.setItem("carrito", JSON.stringify(carrito))
+let cart = [];
+
+const agregarCarrito=(indice)=>{
+    const indiceEncontradoCarrito = cart.findIndex((elemento)=>{
+        return elemento.id === productos[indice].id;
+    });
+    if(indiceEncontradoCarrito === -1){
+        const agregarproducto= productos[indice];
+        agregarproducto.cantidad = 1;
+        cart.push(agregarproducto);
+        actualizarlocalstorage(cart);
+        crearcarrito();
+    }
+    else {
+        cart[indiceEncontradoCarrito].cantidad += 1
+        actualizarlocalstorage(cart);
+        crearcarrito();
+    }
 };
 
 const removeProduct =(indice)=>{
-    carrito.splice(indice, 1);
-    actualizarlocalstorage(carrito);
-    crearcarrito()
+    cart.splice(indice, 1);
+    actualizarlocalstorage(cart);
+    crearcarrito();
 };
 
 const finalizarcompra = ()=> {
-    const finaltotal = document.getElementsByClassName("totaltotal")[0].innerHTML;
-    modalCarrito.innerHTML ="";
+    const finaltotal = document.getElementsByClassName("total-carrito")[0].innerHTML;
+    cuadrocarrito.innerHTML ="";
     const comprafinalizada = `
     <div class="product-details">Total: $ ${total}</div>
     <p class="datos-parrafo"> Ya casi estamos! Decinos donde te entregamos tu nueva notebook.</p>
     <button class="btn btn-info formulario" id="formulario" onClick="crearformulario()"> AGREGAR DIRECCION</button>
     `;
-    modalCarrito.innerHTML = comprafinalizada;
-}
+    cuadrocarrito.innerHTML = comprafinalizada;
+};
+
+const crearformulario =()=> {
+    cuadrocarrito.innerHTML ="";
+    const formulario = `
+    <h2> FORMULARIO DE ENTREGA</h2>
+    <div class="contact__secction-conteiner">
+        <div class="row">
+            <div class="contact__secction__item">
+                <label>Nombre</label>
+                <input type="text" id="nombre" placeholder="Nombre">
+            </div>
+            <div class="contact__secction__item">
+                <label>Dirreccion</label>
+                <input type="text" id="dirreccion" placeholder="Dirreccion">
+            </div>
+            <div class="contact__secction__item">
+                <label>Codigo Postal</label>
+                <input type="text" id="codigopostal" placeholder="Codigo Postal">
+            </div>
+            <div class="contact__secction__item">
+                <label>Telefono</label>
+                <input type="text" id="telefono" placeholder="Telefono">
+            </div>
+            <div class="contact__secction__item">
+                <label>Mail</label>
+                <input type="text" id="mail" placeholder="Mail">
+            </div>
+            <div class="contact-button">
+                <button type="button" class="btn btn-info envio" onClick="msjgracias()"> CONFIRMAR </button>
+            </div>
+        </div>
+    </div>
+    `;
+    cuadrocarrito.innerHTML = formulario;
+};
+
+const msjgracias = () => {
+    const nombreCliente = document.getElementById("nombre").value;
+    const domiciliocliente = document.getElementById("dirreccion").value;
+    cuadrocarrito.innerHTML = "";
+    let mensaje = `
+    <div class="mensaje-final"> ${nombreCliente}, Muchas gracias por tu compra! En 48 horas recibiras tu nueva notebook en ${domiciliocliente}
+</div>
+    `;
+    cuadrocarrito.innerHTML = mensaje;
+};
+
+const actualizarlocalstorage = (cart)=>{
+    localStorage.setItem("cart", JSON.stringify(cart))
+};
+
+
